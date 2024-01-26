43a51dc49e3436454499fc7f5c45fa1270df8cb7
Caleb Lui-Yee <113129278+Waluiyeegi@users.noreply.github.com>
2024-01-26 03:00:06 -0500

    as;ldfjasdlkj;f

--- src/Barista.java
+++ src/Barista.java
@@ -1,26 +1,34 @@
 import java.util.Random;
 
-public class Barista extends Thread{
+public class Barista implements Runnable {
 
     Counter counter;
 
     Ingredients infiniteIngredient;
     IngredientsGiven ingredientsGiven;
 
+    int amountOfCups;
+
     public Barista (Ingredients ingredientsGiven, Counter counter){
         this.counter = counter;
         this.infiniteIngredient = ingredientsGiven;
+        this.amountOfCups = 0;
     }
 
     public void run(){
-        for (int n = 1; n <= 20; n++) {
-
-            while (counter.isEmpty() == true){};
-            if ((counter.seeIngredients().getIngredient1() != infiniteIngredient) && (counter.seeIngredients().getIngredient2() != infiniteIngredient)){
+        while (int n = 1; n <= 20; n++) {
+            //System.out.println("doin \n");
+            while (counter.isEmpty()){};
+            if ((counter.seeIngredients().getIngredient1() != infiniteIngredient) && (counter.seeIngredients().getIngredient2() != infiniteIngredient)) {
+                //System.out.println("doin2 \n");
+                //System.out.println("got the stuff and " + infiniteIngredient.toString() + "\n");
                 ingredientsGiven = counter.get();
-                System.out.println("got the stuff\n");
+                amountOfCups++;
                 //System.out.println("made a drink with " + ingredientsGiven.toString() + " and " + infiniteIngredient.toString() +"\n");
             }
+
+            System.out.println(n + "\n");
+
 
         }
     }
